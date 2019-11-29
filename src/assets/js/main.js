8a03884c9aedd2f66aa9f0012fdea3900a3c802b
serii <seriiburduja@gmail.com>
2019-11-29 10:05:01 +0200

    slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -18,27 +18,29 @@ $(function () {
 	});
 
 	let slider = function () {
-		$('#js-slider').slick({
+		$('#js-slider__wrap').slick({
 			arrows: false,
 			fade: true,
 			responsive: [
 				{
 					breakpoint: 720,
 					settings: {
-						dots: true
+						dots: true,
+						slidesToShow: 1,
+						slidesToScroll: 1
 					}
 				},
 			]
 		});
 
-		$('.slider__button--left').on('click', function (e) {
+		$('.slider-button__left').on('click', function (e) {
 			e.preventDefault();
-			$('#js-slider').slick('slickPrev');
+			$('#js-slider__wrap').slick('slickPrev');
 		});
 
-		$('.slider__button--right').on('click', function (e) {
+		$('.slider-button__right').on('click', function (e) {
 			e.preventDefault();
-			$('#js-slider').slick('slickNext');
+			$('#js-slider__wrap').slick('slickNext');
 		});
 	};
 	slider();
@@ -52,47 +54,6 @@ $(function () {
 		});
 	};
 	sandwitch();
-
-	let tabs = function () {
-		$('#js-tabs a').on('click', function (e) {
-			e.preventDefault();
-			let id = $(this).attr('href');
-			$('#js-tabs a').removeClass('active');
-			$(this).addClass('active');
-			$('#js-tabs-content .tabs-content__item').fadeOut(100);
-			$(id).fadeIn(100);
-		});
-	};
-	tabs();
-
-	let showPopup = function () {
-		$('#js-show-popup').on('click', function (e) {
-			e.preventDefault();
-			$('#js-overlay').fadeIn(100);
-			$('#js-popup').addClass('active');
-		});
-		$('#js-overlay, #js-popup-close').on('click', function () {
-			$('#js-overlay').fadeOut(100);
-			$('#js-popup').removeClass('active');
-		});
-	};
-	showPopup();
-
-	let wow = new WOW(
-		{
-			boxClass: 'wow',      // animated element css class (default is wow)
-			animateClass: 'animated', // animation css class (default is animated)
-			offset: 60,          // distance to the element when triggering the animation (default is 0)
-			mobile: true,       // trigger animations on mobile devices (default is true)
-			live: true,       // act on asynchronously loaded content (default is true)
-			callback: function (box) {
-				// the callback is fired every time an animation is started
-				// the argument that is passed in is the DOM node being animated
-			},
-			scrollContainer: null // optional scroll container selector, otherwise use window
-		}
-	);
-	wow.init();
 });
 
 
